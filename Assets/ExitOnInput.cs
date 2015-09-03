06de1bc69a2254fc607e53c882d10a034be71c4f
John Sietsma <john.sietsma@gmail.com>
2015-09-03 17:20:29 +1000

    First drop.

---
+++ Assets/ExitOnInput.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitOnInput : MonoBehaviour {
+
+    Vector3 mousePos;
+
+    void Start()
+    {
+        mousePos = Input.mousePosition;
+    }
+
+	void Update () {
+
+
+
+        if (Input.anyKeyDown || Input.mousePosition != mousePos)
+        {
+            Application.Quit();
+        }
+
+	}
+}
---
+++ Assets/Orbit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour {
+
+    public float speed;
+
+	void Update () {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.Self);
+	}
+}
